e976f4d6647ac096eed511549418ae304961e927
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-02-07 21:21:08 +0530

    Hint to save and run the code for first time

--- My first program.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"Abhinav You did it Now time to complete this";
-    return 0;
-}
---
+++ My_demo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class play{
+    public:
+    int Roll;
+    int returnroll(){
+        return Roll;
+    }
+};
+int main()
+{
+    play a;
+    cout<<"Enter the roll number of student";
+    cin>>a.Roll;    
+    cout<<"Roll no of the student is "clock_settime<<a.returnroll();
+    return 0;
+}
+
+
+
+/* Hey buddy very tough job was to do all these setup a few thing must to tell you are First is make sure that you click on option 
+   setting and in that you go to run code configurations and there click on allow run in terminal and later after that also and also 
+   remeber to insall extension code runner*/
+   
